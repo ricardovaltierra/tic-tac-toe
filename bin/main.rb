e5deec2955338e5407090812b97f7beb08f1fdc0
firmo holanda <firmo@viize.com>
2019-12-13 14:19:48 -0300

    init player class

--- bin/main.rb
+++ bin/main.rb
@@ -1,1 +1,23 @@
-puts "Hello World!"
+
+class Player
+  
+  def initialize(name, team)
+    @name = name
+    @team = team
+  end
+
+  def name
+    @name
+  end
+
+  def team
+    @team
+  end
+
+end
+
+palyer01 = Player.new("ricardo", "X")
+palyer02 = Player.new("firmo", "O")
+
+#puts palyer01.name
+puts palyer01.team
