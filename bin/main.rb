684e5ef661ba795f8982afa31e1c44f58377eaec
firmo holanda <firmo@viize.com>
2019-12-13 17:59:10 -0300

    Update main.rb

--- bin/main.rb
+++ bin/main.rb
@@ -20,29 +20,30 @@ class Player
 
 end
 
-# Creating an object of the class 
 
-def  get_player_data(player_number)
+def get_player_data
 
-  puts "Hi Player #{player_number}, please type your full name: "
-  player_name = gets.chomp
+  puts "Hi Player 1, please type your full name: "
+  player01_name = gets.chomp
 
-  puts "Thank you, now please the team you want to belong (X / O): "
-  player_team = gets.chomp
-  else
-    if player_team == 'X'
-  end
+  puts "Thank you, now please select the team you want to belong (X / O): "
+  player01_team = gets.chomp.upcase
+  player02_team = player01_team == 'X'   ? 'O' : 'X'
 
-  player = Player.new(player_one_name, player_one_team)
+  puts "Hi Player 2, please type your full name: "
+  player02_name = gets.chomp
 
-  player
+  #puts "#{player01_name}'s team is: #{player01_team}"
+  #puts "#{player02_name}'s team is: #{player02_team}"
 
 end
 
-player_1 = get_player_data(1)
-puts "Hi " + player_1.name + ", you're going to be player number one, and you selected the '"+ player_1.team + "' team"
+get_player_data
 
-player_2 = get_player_data(2)
-puts "\nHi " + player_2.name + ", you're going to be player number one, and you selected the '"+ player_2.team + "' team"
+# Creating an object of the class
+  player01 = Player.new(player01_name, player01_team)
+  player02 = Player.new(player02_name, player02_team)
 
-$p2.team = $p1.team == 'X' ? 'O' : 'X' 
+
+puts player01.name
+puts player01.team
