50a27146075af24e17af11bcb96d251f25d648b0
ricardovaltierra <ricardo_valtierra@outlook.com>
2019-12-16 12:34:07 -0600

    'game' class added to 'game_logic.rb'

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -46,4 +46,62 @@ class Board
     board.none? {|x| x == "-"}
   end
 
+# game -------------------------------------------------------------------------------- #
+class Game
+  attr_reader :board, :user_interface, :players
+
+  def initialize(board = Board.new, user_interface = UserInterface.new, players = [Player.new, Player.new], game_on = true)
+    @board = board
+    @user_interface = user_interface
+    @players = players
+    @game_on = game_on
+  end
+
+  def create_players
+    names = user_interface.get_names(players)
+    players.each_with_index {|player, i| player.name = names[i]}
+  end
+
+  def set_piece
+    p_team = ["X","O"].shuffle
+    players[0].team = p_team[0]
+    players[1].team = p_team[1]
+    user_interface.assign_piece(players)
+  end
+
+  def select_first_player
+    players.shuffle!
+    user_interface.first_turn(players[1])
+  end
+
+  def make_turn
+    turn = user_interface.get_turn(players[0], board)
+    board.take_turn(turn, players[0].team)
+  end
+
+  def intro
+    user_interface.instructions
+    create_players
+    set_piece
+    select_first_player
+  end
+
+  def game_over?(input)
+    board.tie? || board.win?(input)
+  end
+
+
+  def play
+    intro
+    #while @game_on
+      until game_over?(players[0].team)
+        players.reverse!
+        make_turn
+        user_interface.display(board.board)
+      end
+      user_interface.declare_win(players[0], board)
+      #user_interface.play_new_game?
+    #end
+  end
+  
 end
