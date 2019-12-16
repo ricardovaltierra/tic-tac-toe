2f70d6988a503e1038d14f00174eba1b15b9ecb6
ricardovaltierra <52725285+ricardovaltierra@users.noreply.github.com>
2019-12-16 12:59:48 -0600

    'user-interface' /  'development' issue solved
    Resolving conflicts between 'user-interface' and 'development' adding the following lines
    
    # frozen_string_literal: true

--- bin/main.rb
+++ bin/main.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ruby
 
+# frozen_string_literal: true
+
 require '../lib/game_logic.rb'
 
 # UserInterface --------------------------------------------------------------------------- #
