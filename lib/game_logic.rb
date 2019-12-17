83f949bd3a9b194a2c6f87702fc19b5d8e22e409
firmo holanda <firmo@viize.com>
2019-12-17 17:48:41 -0300

    refactored game_over?

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -91,18 +91,14 @@ class Game
     select_first_player
   end
 
-  def game_is_tie?
-    board.tie?
-  end
-
-  def player_win?(input)
-    board.win?(input)
+  def game_over?(input)
+    board.tie? || board.win?(input)
   end
 
   public
   def play
     intro
-    until (game_is_tie? || player_win?(players[0].team)) 
+    until game_over?(players[0].team)
       players.reverse!
       make_turn
       user_interface.display(board.board)
